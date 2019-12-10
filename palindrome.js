b74c0f63da1331e776793a03e3bdb8c70e9a7d14
Alina <alina@Apples-MacBook-Pro.local>
2019-12-10 05:01:30 +0300

    Первая попытка

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,47 @@
-function runPalindromes() {
+function replaseSymbol(text, symb, sub = '') {
+    while (text.indexOf(symb) != -1) {
+        text = text.replase(symb, sub)
+    }
+    return text;
+}
+
+function isPalindrome(text) {
+    text = text.toLowerCase();
+    text = replaseSymbol(text, ' ');
+    text = replaseSymbol(text, 'ё', 'е');
+    text = replaseSymbol(text, 'ь');
+    text = replaseSymbol(text, 'ъ');
+    text = replaseSymbol(text, '.');
+    text = replaseSymbol(text, ',');
+    text = replaseSymbol(text, '!');
+    text = replaseSymbol(text, '?');
+    text = replaseSymbol(text, '-');
+    text = replaseSymbol(text, ':');
+    text = replaseSymbol(text, ';');
+    text = replaseSymbol(text, '(');
+    text = replaseSymbol(text, ')');
+    text = replaseSymbol(text, '"');
+    text = replaseSymbol(text, '/');
+    text = replaseSymbol(text, '{');
+    text = replaseSymbol(text, '}');
+    
+    let reverseText = '';
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        reverseText += text.charAt(i);
+    }
+
+    if (text === reverseText) {
+        return 'палиндром';
+    } 
+    return 'не палиндром';
+}
+
+
+
+
+
+function runPalindrome() {
     let userText = prompt('Введите фразу');
-    console.log((userText))
+    console.log('Фраза ' + '' + ' - это ' + isPalindrome(userText))
 }
